c7eb6f0661cf1f4cc847fac0f210efa57fa73709
leonidios <programertv633@gmail.com>
2020-08-31 01:41:09 +0300

    Added comment creation

--- main/consumers.py
+++ main/consumers.py
@@ -1,7 +1,7 @@
 import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
-from .models import Article,User
+from .models import Article,User,comment
 
 class ArticleConsumer(WebsocketConsumer):
     def connect(self):
@@ -45,3 +45,50 @@ class ArticleConsumer(WebsocketConsumer):
     def vote(self,event):
         message = json.dumps(event['message'])
         self.send(message)
+
+class ArticleDetailConsumer(WebsocketConsumer):
+
+    def connect(self):  
+        ids = [item for item in self.scope['path'].split("/") if item.strip() != ''][-1]
+        article = Article.objects.filter(pk=ids)
+        self.is_authenticated = self.scope['user'].is_authenticated
+
+        if self.is_authenticated:
+            self.user = User.objects.filter(username=self.scope["user"]).first()
+
+        if article.exists(): 
+            print("Row detected in database")
+            self.connection = "art_{}".format(ids)
+            self.article = article.first()
+            async_to_sync(self.channel_layer.group_add)(
+                self.connection,
+                self.channel_name
+            )
+            self.accept()
+    
+    def receive(self,text_data):
+        if not self.is_authenticated:
+            self.disconnect()
+            return None
+        data = json.loads(text_data)
+        if data['data'].strip() != '':
+            if data['type'] == 'comment':
+                com = comment(creator=self.user,description=data['data'],is_reply=data['is_reply'])
+                com.save()
+                self.article.comments.add(com)
+                async_to_sync(self.channel_layer.group_send)(
+                    self.connection,
+                    {
+                        'type' : 'comment',
+                        'message' : {
+                            'img' : self.user.userprofile.image,
+                            'msg' : com.description,
+                            'date_created' : com.date_created
+                        }
+                    }
+                )
+
+    def comment(self,event):
+        msg = event['message']
+        self.send(msg)
+    
--- main/routing.py
+++ main/routing.py
@@ -3,4 +3,5 @@ from django.urls import re_path
 
 websocket_urlpatterns = [
     re_path(r'ws/articles/', consumers.ArticleConsumer,name='article_update'),
+    re_path(r'ws/posts/\w+/$',consumers.ArticleDetailConsumer,name='article_detail')
 ]
